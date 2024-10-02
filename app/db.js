4a5e9d14e3c5248e69dee48c16fdc31b7d565f88
Matías Díaz <mati98ld@gmail.com>
2024-10-02 11:39:32 -0300

    Configuraciones

--- app/db.js
+++ app/db.js
@@ -1,10 +1,11 @@
 import { connect } from 'mongoose'
+import { config as dotenv } from 'dotenv'
+
+dotenv()
 
 export const connectDB = async () => {
   try {
-    await connect(
-      'mongodb+srv://mati98ld:lola3085@juegojsdb.b8lin.mongodb.net/?retryWrites=true&w=majority&appName=JuegoJSDB',
-    )
+    await connect(process.env.MONGODB_URI)
     console.log('Conexión a la base de datos exitosa')
   } catch (error) {
     console.error('Error al conectar a la base de datos', error)
